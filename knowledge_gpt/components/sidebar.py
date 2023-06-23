13d6ced14ba0df80fba48bef704703b5c28a65f9
Josh Studl <josh_studl@innovationfirst.com>
2023-06-23 11:39:01 -0400

    refactor

--- knowledge_gpt/components/sidebar.py
+++ knowledge_gpt/components/sidebar.py
@@ -26,18 +26,18 @@ def sidebar():
         if api_key_input:
             set_openai_api_key(api_key_input)
 
-        st.markdown("---")
-        st.markdown("# About")
-        st.markdown(
+      #  st.markdown("---")
+      #  st.markdown("# About")
+      #  st.markdown(
             "📖KnowledgeGPT allows you to ask questions about your "
             "documents and get accurate answers with instant citations. "
-        )
-        st.markdown(
+     #   )
+      #  st.markdown(
             "This tool is a work in progress. "
             "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
             "with your feedback and suggestions💡"
-        )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
-        st.markdown("---")
+    #    )
+      #  st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+    # st.markdown("---")
 
-        faq()
+#        faq()
