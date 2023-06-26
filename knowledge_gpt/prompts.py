d4c198b71b9fdc0e2592cf7fa7c48ee7d875e576
Josh Studl <josh_studl@innovationfirst.com>
2023-06-26 19:23:19 -0400

    new prompts

--- knowledge_gpt/prompts.py
+++ knowledge_gpt/prompts.py
@@ -6,16 +6,18 @@ template = """Create a final answer to the given questions using the provided do
 
 ---------
 
-QUESTION: What  is the purpose of ARPA-H?
+QUESTION: What are the benefits of VEX STEM Labs?
 =========
-Content: More support for patients and families. \n\nTo get there, I call on Congress to fund ARPA-H, the Advanced Research Projects Agency for Health. \n\nIt’s based on DARPA—the Defense Department project that led to the Internet, GPS, and so much more.  \n\nARPA-H will have a singular purpose—to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
+Content: "Learning coding and computational thinking can have a positive effect on students' cognitive, social, and emotional development"
 Source: 1-32
-Content: While we’re at it, let’s make sure every American can get the health care they need. \n\nWe’ve already made historic investments in health care. \n\nWe’ve made it easier for Americans to get the care they need, when they need it. \n\nWe’ve made it easier for Americans to get the treatments they need, when they need them. \n\nWe’ve made it easier for Americans to get the medications they need, when they need them.
+Content: "Literacy-themed curricular resources for VEX 123 can be found in the form of VEX 123 Activities, Activity Series, and VEX 123 STEM Labs"
 Source: 1-33
-Content: The V.A. is pioneering new ways of linking toxic exposures to disease, already helping  veterans get the care they deserve. \n\nWe need to extend that same care to all Americans. \n\nThat’s why I’m calling on Congress to pass legislation that would establish a national registry of toxic exposures, and provide health care and financial assistance to those affected.
+Content: "The Essential Questions and Unit Understandings in a STEM Lab are woven throughout its activities and assessments to help you support student learning."
 Source: 1-30
+Content: "The Alternate Coding Methods section in the STEM Lab Unit Overview provides information on how to teach STEM Labs using different ways to code"
+Content: "VEX removes typical impediments to students learning Computer Science."
 =========
-FINAL ANSWER: The purpose of ARPA-H is to drive breakthroughs in cancer, Alzheimer’s, diabetes, and more.
+FINAL ANSWER: "VEX STEM Labs can be used to support social-emotional curriculum in your classroom."
 SOURCES: 1-32
 
 ---------
